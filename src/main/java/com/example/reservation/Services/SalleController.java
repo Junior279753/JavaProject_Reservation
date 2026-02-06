9f41f8d4b388c84fc3080f3b7b324ab4971f7d51
Junior279753 <juniorazonnoudo2@gmail.com>
2026-02-06 10:00:42 +0100

    Refactor SalleController to improve error handling and remove unused annotations

--- src/main/java/com/example/reservation/Services/SalleController.java
+++ src/main/java/com/example/reservation/Services/SalleController.java
@@ -3,21 +3,14 @@ package com.example.reservation.Services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.reservation.Entities.Salle;
 import com.example.reservation.Repositories.SalleRepository;
 
-@RestController
-@RequestMapping("/api/salles")
+@Service
 public class SalleController {
 	
 	@Autowired
@@ -25,17 +18,19 @@ public class SalleController {
 	
 
 	public List <Salle> getAllSalle(){
-		return salleRepo.findAll();
+		List<Salle> salles = salleRepo.findAll();
+	    
+	    if (salles.isEmpty()) {
+	        throw new RuntimeException("No salles found");
+	    }
+	    
+	    return salles;
 	}
 	
+	
 
-	public ResponseEntity<?> addSalle(@RequestBody Salle salle) {
-		
-		try {
-			return ResponseEntity.ok(salleRepo.save(salle));
-		}catch (Exception e) {
-	        return ResponseEntity.badRequest().body("Error : La salle n'a pas pu être créer ");
-	    }
+	public Salle addSalle(@RequestBody Salle salle) {
+		return salleRepo.save(salle);
 	}
 	
 
